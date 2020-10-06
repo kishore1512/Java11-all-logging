9b203817fc8a71168a085ac046ddc2a0975caa8e
kishore1512 <52534799+kishore1512@users.noreply.github.com>
2020-10-06 21:33:39 +0530

    Update 02_newrelic-apm.sh

--- .platform/hooks/prebuild/02_newrelic-apm.sh
+++ .platform/hooks/prebuild/02_newrelic-apm.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-sed -i -e 's/app_name.*/app_name: "'$NR_Key'"/' newrelic/newrelic.yml
-sed -i -e 's/license_key.*/license_key: '"'$NR_APM'"'/' newrelic/newrelic.yml
+sed -i -e 's/app_name.*/app_name: "'$NR_APM'"/' newrelic/newrelic.yml
+sed -i -e 's/license_key.*/license_key: '"'$NR_Key'"'/' newrelic/newrelic.yml
